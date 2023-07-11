1293c1c80005bc584bff55cd45f06cb27ba3d514
JacStr <jack.strange@softwire.com>
2023-07-11 14:47:37 +0100

    Implement stops dropdown

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,19 +1,66 @@
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { useState } from 'react'
+import { Linking, StyleSheet, View } from 'react-native'
+import { Button } from 'react-native-paper'
+import DropDown from 'react-native-paper-dropdown'
 
-const DetailsScreen = () => (
-  <View style={styles.container}>
-    <Text>Details Screen</Text>
-  </View>
-)
-
-export default DetailsScreen
+export default function DetailsScreen() {
+  const [showDropDown, setShowDropDown] = useState(false)
+  const [stop, setStop] = useState('')
+  const stops = [
+    {
+      label: 'Piccadilly Gardens',
+      value:
+        'https://tfgm.com/public-transport/tram/stops/piccadilly-gardens-tram',
+    },
+    {
+      label: 'Firswood',
+      value: 'https://tfgm.com/public-transport/tram/stops/firswood-tram',
+    },
+    {
+      label: 'Sale Water Park',
+      value:
+        'https://tfgm.com/public-transport/tram/stops/sale-water-park-tram',
+    },
+    {
+      label: 'Parkway',
+      value: 'https://tfgm.com/public-transport/tram/stops/parkway-tram',
+    },
+    {
+      label: 'Rochdale Railway Station',
+      value:
+        'https://tfgm.com/public-transport/tram/stops/rochdale-railway-station-tram',
+    },
+  ]
+  return (
+    <View style={styles.container}>
+      <DropDown
+        label={'Stops'}
+        mode={'outlined'}
+        visible={showDropDown}
+        showDropDown={() => setShowDropDown(true)}
+        onDismiss={() => setShowDropDown(false)}
+        value={stop}
+        setValue={setStop}
+        list={stops}
+      />
+      <View style={{ padding: '2%' }}></View>
+      <Button
+        mode="outlined"
+        onPress={() => {
+          void Linking.openURL(stop)
+        }}
+      >
+        Find Times
+      </Button>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
+    width: '80%',
+    alignSelf: 'center',
     justifyContent: 'center',
   },
 })
