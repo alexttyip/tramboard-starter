2691f2454b47040c4b1cf3793c2335b175c93ee6
JacStr <jack.strange@softwire.com>
2023-07-11 16:36:10 +0100

    Filter API data on selection

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,36 +1,60 @@
 import { useState } from 'react'
-import { Linking, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Button } from 'react-native-paper'
 import DropDown from 'react-native-paper-dropdown'
+import { config } from '../config'
+
+const tfgmEndpoint = 'https://api.tfgm.com/odata/Metrolinks'
+const stops = [
+  {
+    label: 'Piccadilly Gardens',
+    value: '9400ZZMAPGD',
+  },
+  {
+    label: 'Firswood',
+    value: '9400ZZMAFIR',
+  },
+  {
+    label: 'Sale Water Park',
+    value: '9400ZZMASWP',
+  },
+  {
+    label: 'Parkway',
+    value: '9400ZZMAPAR',
+  },
+  {
+    label: 'Rochdale Railway Station',
+    value: '9400ZZMARRS',
+  },
+]
 
 export default function DetailsScreen() {
   const [showDropDown, setShowDropDown] = useState(false)
   const [stop, setStop] = useState('')
-  const stops = [
-    {
-      label: 'Piccadilly Gardens',
-      value:
-        'https://tfgm.com/public-transport/tram/stops/piccadilly-gardens-tram',
-    },
-    {
-      label: 'Firswood',
-      value: 'https://tfgm.com/public-transport/tram/stops/firswood-tram',
-    },
-    {
-      label: 'Sale Water Park',
-      value:
-        'https://tfgm.com/public-transport/tram/stops/sale-water-park-tram',
-    },
-    {
-      label: 'Parkway',
-      value: 'https://tfgm.com/public-transport/tram/stops/parkway-tram',
-    },
-    {
-      label: 'Rochdale Railway Station',
-      value:
-        'https://tfgm.com/public-transport/tram/stops/rochdale-railway-station-tram',
-    },
-  ]
+
+  function handleClick() {
+    fetch(tfgmEndpoint, {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': config.apiKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => filterJson(json as { value: [{ AtcoCode: string }] }))
+      .then((filteredJson) =>
+        console.log(JSON.stringify(filteredJson, null, 2))
+      )
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  function filterJson(json: { value: [{ AtcoCode: string }] }) {
+    return json.value.filter((apiStop) => {
+      return apiStop.AtcoCode.includes(stop)
+    })
+  }
+
   return (
     <View style={styles.container}>
       <DropDown
@@ -44,12 +68,7 @@ export default function DetailsScreen() {
         list={stops}
       />
       <View style={{ padding: '2%' }}></View>
-      <Button
-        mode="outlined"
-        onPress={() => {
-          void Linking.openURL(stop)
-        }}
-      >
+      <Button mode="outlined" onPress={handleClick}>
         Find Times
       </Button>
     </View>
@@ -59,7 +78,7 @@ export default function DetailsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '80%',
+    width: 300,
     alignSelf: 'center',
     justifyContent: 'center',
   },
