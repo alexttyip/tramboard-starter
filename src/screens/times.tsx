4c22bb4ba9403a021bedbde0281cf9b00075b456
Georgia Moore <georgia.moore@softwire.com>
2023-10-18 15:38:14 +0100

    P1: Add dropdown component for stop list

--- src/screens/times.tsx
+++ src/screens/times.tsx
@@ -1,11 +1,41 @@
+import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text } from 'react-native-paper'
+import DropDown from 'react-native-paper-dropdown'
 
-const TimesScreen = () => (
-  <View style={styles.container}>
-    <Text>Times Screen</Text>
-  </View>
-)
+const TimesScreen = () => {
+  const [showDropDown, setShowDropDown] = React.useState(false)
+  const [stop, setStop] = React.useState('')
+  const stopList = [
+    {
+      label: 'Stop 1',
+      value: '1',
+    },
+    {
+      label: 'Stop 2',
+      value: '2',
+    },
+    {
+      label: 'Stop 3',
+      value: '3',
+    },
+  ]
+  return (
+    <View style={styles.container}>
+      <Text>Times Screen</Text>
+      <DropDown
+        label={'Stops'}
+        mode={'outlined'}
+        visible={showDropDown}
+        showDropDown={() => setShowDropDown(true)}
+        onDismiss={() => setShowDropDown(false)}
+        value={stop}
+        setValue={setStop}
+        list={stopList}
+      />
+    </View>
+  )
+}
 
 export default TimesScreen
 
